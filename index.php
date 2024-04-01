397d3f714761f0c6cc4e2f89d9bd54ed48a46396
Weslly <weslly1234vitor@gmail.com>
2024-04-01 02:01:58 -0300

    :rocket: Criando Função para calculo do IMC

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
+<?php
+
+function calcularGorduraCorporal($sexo, $cintura, $altura, $pescoco = 0, $quadril = 0) {
+    if ($sexo == "masculino") {
+        return 495 / (1.0324 - 0.19077 * (log10($cintura - $pescoco)) + 0.15456 * (log10($altura*100))) - 450;
+    } elseif ($sexo == "feminino") {
+        return 495 / (1.29579 - 0.35004 * (log10($cintura + $quadril - $pescoco)) + 0.22100 * (log10($altura*100))) - 450;
+    } else {
+        return null; 
+    }
+}
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
